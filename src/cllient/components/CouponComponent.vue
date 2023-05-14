26d0b00b274e091908e924661fab94eeb38d1b36
Mitopek1996 <michaltopor1996@gmail.com>
2023-05-14 23:14:59 +0200

    feat: coupon component

---
+++ src/cllient/components/CouponComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="coupon-component">
+    <span>Rozpoczęcie: <span class="start-date">{{props.startDate}}</span></span>
+    <div class="image-wrapper">
+      <img :src="props.imageUrl" alt="" class="img"/>
+    </div>
+    <div class="text-container">
+      <span class="description" v-if="description">{{props.description}}</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+  imageUrl: string
+  startDate: Date
+  description?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  description: '',
+})
+
+</script>
+
+<style scoped>
+.image-wrapper{
+  width: 300px;
+}
+
+.start-date{
+  font-weight: 600;
+  letter-spacing: 1px;
+}
+
+.img{
+  border: 1px solid black;
+}
+
+.description{
+  font-size: 14px;
+}
+
+</style>
--- src/cllient/components/pages/CouponsPage.vue
+++ src/cllient/components/pages/CouponsPage.vue
@@ -11,8 +11,8 @@
               <CouponsOptions/>
             </div>
           </div>
-          <div>
-            ss
+          <div class="content">
+            <CouponComponent startDate="2023-05-03 18:00" description="No tak mysle ze to tak ale moze jednak nie" imageUrl="https://scontent-waw1-1.xx.fbcdn.net/v/t39.30808-6/346089297_3371475173126792_8081334352204991755_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=h1l6eeprQbcAX9UQxF9&_nc_ht=scontent-waw1-1.xx&oh=00_AfCpQ2_orBm3bT6GZdJu3nz-Bi2pu3JgQ4jUjyzSl9y9jg&oe=64667737"/>
           </div>
         </div>
       </div>
@@ -36,11 +36,12 @@ onMounted(() => {
 import DefaultLayout from "../DefaultLayout.vue";
 import CouponsOptions from "../CouponsOptions.vue";
 import SubscriptionTime from "../SubscriptionTime.vue";
+import CouponComponent from "../CouponComponent.vue";
 </script>
 
 <style scoped>
 .main-container{
-  margin-top: 50px;
+  margin-top: 40px;
   padding: 30px;
   display: flex;
   justify-content: center;
@@ -48,6 +49,10 @@ import SubscriptionTime from "../SubscriptionTime.vue";
 
 .subscription-time-wrapper{
   padding: 15px 0 0 15px;
+}
+
+.content{
+  padding: 30px;
 }
 
 .options{
@@ -63,11 +68,11 @@ import SubscriptionTime from "../SubscriptionTime.vue";
 
 .panel{
   border-radius: 10px;
-  background-color: hsla(0deg,0%,10%,.85);
+  background-color: hsla(0deg,0%,11%,.90);
   grid-template-columns: 250px 1fr;
   display: grid;
   width: 70%;
-  min-height: 80vh;
+  min-height: 90vh;
 }
 
 </style>
