0c349c8238c806fe374e122d54c042f6ec55876c
Mitopek1996 <michaltopor1996@gmail.com>
2023-12-15 01:58:33 +0100

    test

--- src/server/controllers/PaymentController/PaymentController.ts
+++ src/server/controllers/PaymentController/PaymentController.ts
@@ -37,29 +37,29 @@ export class PaymentController implements IPaymentController {
     const password = process.env.HOTPAY_PASSWORD
     const secret = process.env.HOTPAY_SECRET
     const {SEKRET, KWOTA, STATUS, ID_ZAMOWIENIA, ID_PLATNOSCI, SECURE, HASH} = req.body
-    console.info(ID_ZAMOWIENIA)
-    const expectedHash = await this.generateSHA256(`${password};${KWOTA};${ID_PLATNOSCI};${ID_ZAMOWIENIA};${STATUS};${secret}`)
-    if(expectedHash !== HASH){
-      return res.sendSuccessResponse('Nope')
-    }
-    await this.paymentRepository.updatePaymentStatus(ID_ZAMOWIENIA, STATUS)
-    if(STATUS === PaymentStatus.SUCCESS){
-      const payment = await this.paymentRepository.findPaymentByExternalId(ID_ZAMOWIENIA)
-      const user = await this.userRepository.findUserById(payment.userId)
-      const subscriptionExpiresAt = user.subscriptionExpiresAt || new Date()
-      let subscriptionDays = 30
-      console.info(payment)
-      if(payment.subscriptionId === 2){
-        subscriptionDays = 183
-      }
-      if(payment.subscriptionId === 3){
-        subscriptionDays = 366
-      }
-      const newSubscriptionExpiresAt = new Date(subscriptionExpiresAt.getTime() + subscriptionDays * 24 * 60 * 60 * 1000)
-      await this.userRepository.updateUserById(user.id, {
-        subscriptionExpiresAt: newSubscriptionExpiresAt
-      })
-    }
+    console.info(SEKRET, KWOTA, STATUS, ID_ZAMOWIENIA, ID_PLATNOSCI, SECURE, HASH)
+    // const expectedHash = await this.generateSHA256(`${password};${KWOTA};${ID_PLATNOSCI};${ID_ZAMOWIENIA};${STATUS};${secret}`)
+    // if(expectedHash !== HASH){
+    //   return res.sendSuccessResponse('Nope')
+    // }
+    // await this.paymentRepository.updatePaymentStatus(ID_ZAMOWIENIA, STATUS)
+    // if(STATUS === PaymentStatus.SUCCESS){
+    //   const payment = await this.paymentRepository.findPaymentByExternalId(ID_ZAMOWIENIA)
+    //   const user = await this.userRepository.findUserById(payment.userId)
+    //   const subscriptionExpiresAt = user.subscriptionExpiresAt || new Date()
+    //   let subscriptionDays = 30
+    //   console.info(payment)
+    //   if(payment.subscriptionId === 2){
+    //     subscriptionDays = 183
+    //   }
+    //   if(payment.subscriptionId === 3){
+    //     subscriptionDays = 366
+    //   }
+    //   const newSubscriptionExpiresAt = new Date(subscriptionExpiresAt.getTime() + subscriptionDays * 24 * 60 * 60 * 1000)
+    //   await this.userRepository.updateUserById(user.id, {
+    //     subscriptionExpiresAt: newSubscriptionExpiresAt
+    //   })
+    // }
     return res.sendSuccessResponse(null)
   }
 
